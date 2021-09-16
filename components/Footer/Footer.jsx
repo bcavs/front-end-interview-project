346cfb65930c96dbc8c701f408b03fdbec272a58
bcavs <cavenagh.ben@gmail.com>
2021-09-15 20:30:01 -0500

    add footer

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+//import { Test } from './Footer.styles';
+
+const Footer = (props) => (
+  <div>
+    Footer..
+  </div>
+);
+
+Footer.propTypes = {
+  // bla: PropTypes.string,
+};
+
+Footer.defaultProps = {
+  // bla: 'test',
+};
+
+export default Footer;
